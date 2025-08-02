e2966af2074b00abf4779b5e9d25c5607b180f57
akash1234 <akash.jannat@gmail.com>
2025-08-02 12:21:15 +0530

    Changed nixos ip

--- 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
+++ 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
@@ -21,12 +21,12 @@
     interfaces.ens18 = {
       useDHCP = false;
       ipv4.addresses = [{
-        address = "10.42.37.100";
+        address = "192.168.100.55";
         prefixLength = 24;
       }];
     };
-    defaultGateway = "10.42.37.254";
-    nameservers = [ "10.42.37.254" ];
+    defaultGateway = "192.168.100.254";
+    nameservers = [ "192.168.100.254" ];
   };
 
   # System localization
